56eb58e490a2499efe4ee5809807c141a7391f4a
samusorcio-create <samu.sorcio@gmail.com>
2026-02-05 11:41:27 +0100

    V 1.1 add class "aquisto"

---
+++ aquisto.py
@@ -0,0 +1,15 @@
+class Aquisto:
+
+        def __init__(self, prezzo, reteizzato, data):
+                self.prezzo = prezzo
+                self.reteizzato = reteizzato
+                self.data = data    
+
+        def __repr__(self):
+            return f"pezzo =>{self.prezzo}, reteizzato =>{self.reteizzato}, data =>{self.data}"
+            
+        def __eq__(self, other):
+            if not isinstance(other, Aquisto):
+                return False    
+            return self.prezzo == other.prezzo and self.reteizzato == other.reteizzato and self.data == other.data
+         
