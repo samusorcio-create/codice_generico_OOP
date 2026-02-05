f2a40aaf0cda3782f8f3e87d80f761d436fac1fa
samusorcio-create <samu.sorcio@gmail.com>
2026-02-05 12:07:31 +0100

    V 1.3 add new class "Torneo"

---
+++ torneo.py
@@ -0,0 +1,16 @@
+class Torneo:
+
+    def __init__(self, nome, data, num_partecipanti, quota_iscrizione, premio):
+        self.nome = nome
+        self.data = data
+        self.num_partecipanti = num_partecipanti
+        self.quota_iscrizione = quota_iscrizione
+        self.premio = premio
+
+    def __ep__(self, other):
+        if not isinstance(other, Torneo):
+            return False
+        return self.nome == other.nome and self.data == other.data and self.num_partecipanti == other.num_partecipanti and self.quota_iscrizione == other.quota_iscrizione and self.premio == other.premio
+    
+    def __repr__(self):
+        return f"nome => {self.nome}, data => {self.data}, numnero dei partecipanti => {self.num_partecipanti}, quota di iscrizione => {self.quota_iscrizione}, premio => {self.premio}"
